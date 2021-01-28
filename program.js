2c5488f0bf03c628281da78d7f525212c19f47eb
Long minh <longminh207@gmail.com>
2021-01-28 16:04:23 +0700

    code thieu nhi lan 2

--- program.js
+++ program.js
@@ -1,8 +1,23 @@
 // Add new functions, variables here
+function Prime(n) {
+  let kq = [];
+  for (i = 2; i < n; i++) {
+    while (n > 1) {
+      if (n % i == 0) {
+        kq.push(i);
+        n = n / i;
+      } else {
+        i++;
+      }
+    }
+  }
+  return kq;
+}
+
 
 function main(input) {
-  // Your code goes here
-  // Using console.log to answer the question
+  let n = parseInt(input);
+  console.log(Prime(n).join(" "));
 }
 
 module.exports = main;
